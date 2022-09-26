45c684c63b4e17a94ab5bdf5c14e85123e0acff2
unit1789 <unit1789@gmail.com>
2022-09-26 09:50:36 -0400

    Migrations

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     
     'taggit',
+    'django_countries',
 
     'users',
     'ecoles',
--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django_countries.fields import CountryField
 
 
-# class TwitterHandle(models.Model):
-#     twitter_handle = models.CharField(max_length=256, blank=True, null=True)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="user_twitter_handle")
 
-#     def __str__(self):
-#         return f"Twitter handle: @{self.twitter_handle}"
+class TwitterHandle(models.Model):
+    twitter_handle = models.CharField(max_length=256, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="user_twitter_handle")
+
+    def __str__(self):
+        return f"Twitter handle: @{self.twitter_handle}"
 
 
 class Profile(models.Model):
@@ -16,8 +18,8 @@ class Profile(models.Model):
     credits = models.IntegerField(default=0, null=False)
     verified_course_creator = models.BooleanField(default=False)
     select_view_seen = models.BooleanField(default=False, null=False, blank=False) # Has user seen select view?
-    # primary_twitter_handle = models.ForeignKey('users.TwitterHandle', on_delete=models.CASCADE, blank=True, null=True)
-    # country = models.CharField(CHOICES)
+    primary_twitter_handle = models.ForeignKey(TwitterHandle, on_delete=models.CASCADE, blank=True, null=True)
+    country = CountryField(default='US')
 
     def __str__(self):
         return f'{self.user.username} Profile'
