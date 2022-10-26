416fd1a26fb4f1d8cec39689c409e735d695d478
bml74 <bml74@georgetown.edu>
2022-10-26 16:57:31 -0400

    Remove majority of old messaging functionality

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import Inbox, UserSearch, Directs, NewConversation, SendDirect
+from . import views
+
+
 urlpatterns = [
-   	path('', Inbox, name='inbox'),
-   	path('directs/<username>/', Directs, name='directs'),
-   	path('new/', UserSearch, name='usersearch'),
-   	path('new/<username>/', NewConversation, name='newconversation'),
-   	path('send/', SendDirect, name='send_direct'),
+   	path('', views.inbox, name='inbox'),
 
 ]
