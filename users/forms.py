379beb1531aa0b6916949ab467e1f94520b008c3
bml74 <bml74@georgetown.edu>
2023-03-17 14:29:28 -0400

    Commit

--- users/forms.py
+++ users/forms.py
@@ -24,7 +24,7 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['description', 'institution', 'country', 'visibility']
+        fields = ['description', 'country', 'visibility']
 
 
 class ReferralCodeForm(forms.Form):
